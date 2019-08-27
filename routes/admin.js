bfdae74dc9c18c8f4e55bf9a50b6c7e4c0ef4d85
MarcoRy <rydzewski.marek86@gmail.com>
2019-08-27 19:15:43 +0200

    Display added news. Added ability to delete news.

--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,12 @@ router.all('*', (req, res, next) => {
 });
 
 router.get('/admin', (req, res) => {
-    res.render('admin/index', {title: 'Admin'});
+    NewsModel.find({}, (err, data) => {
+        if (err) {
+            console.log('I can not find News', err)
+        }
+        res.render('admin/index', {title: 'Admin', data});
+    })
 });
 
 router.get('/news/add', (req, res) => {
@@ -20,21 +25,35 @@ router.get('/news/add', (req, res) => {
 });
 
 router.post('/news/add', (req, res) => {
-    const body = req.body;
+    let information = '';
+    let body = req.body;
     const newsModel = new NewsModel(req.body);
     let errors = newsModel.validateSync();
     if (errors === undefined) {
         errors = [];
     }
 
-    newsModel.save(err => {
+    newsModel.save((err) => {
         if (err) {
             console.log('Fail during save to DB');
         } else {
+            body = {};
+            information = 'News is added';
             console.log('Save to DB');
         }
+        res.render('admin/news-form', {title: 'Add news to site', errors, body, information})
     });
-
-    res.render('admin/news-form', {title: 'Add news to site', errors, body})
 });
+
+router.get('/news/delete/:id', (req, res) => {
+    NewsModel.findByIdAndDelete({_id: req.params.id}, (err) => {
+        if (err) {
+            console.log('I can not delete this news');
+        } else {
+            console.log('News is deleted');
+            res.redirect('/admin');
+        }
+    });
+});
+
 module.exports = router;
