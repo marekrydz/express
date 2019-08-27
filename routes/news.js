54905b714aa23357a79ca1fbfd4e779684179f77
MarcoRy <rydzewski.marek86@gmail.com>
2019-08-27 20:02:42 +0200

    Display news list in label news with sort desc by date

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const NewsModel = require('../models/NEWS');
+
 
 /* GET home page. */
 router.get('/news', (req, res) => {
-    res.render('news', {title: 'News'});
+    const news = NewsModel.find();
+    news.sort({date: 'desc'});
+
+    news.exec((err, data) => {
+        if (err) {
+            console.log('Problem with list in News label', err)
+        } else {
+            res.render('news', {title: 'News', data});
+        }
+    });
+
 });
 
 module.exports = router;
