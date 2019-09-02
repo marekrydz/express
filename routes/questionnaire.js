929b3bd07a0239cb7330a65313fea45290acea32
MarcoRy <rydzewski.marek86@gmail.com>
2019-09-02 22:06:30 +0200

    Displaying answer statistic after answer a question

--- routes/questionnaire.js
+++ routes/questionnaire.js
@@ -1,24 +1,61 @@
 const express = require('express');
 const QuestionnairesModel = require('../models/QUESTIONNARIE');
 const router = express.Router();
-
 let question = 'What is yours favorite season';
 let voteAdded = 'You send your vote before';
 let isVoteButton = true;
 
+
 /* GET home page. */
 router.get('/questionnaire', (req, res) => {
-    isVoteButton = true;
-    voteAdded = "";
-    console.log(isVoteButton);
-    QuestionnairesModel.find({}, (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('questionnaire', {title: 'Questionnaire', data, voteAdded, question, isVoteButton});
-        }
-    });
+    const sameUser = req.session.questionnarie;
+    if (sameUser === 1) {
+        console.log(voteAdded);
+        isVoteButton = false;
+        question = "";
+        QuestionnairesModel.find({}, (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                let sum = 0;
+                data.forEach(answer => {
+                    sum += answer.vote;
+                });
+
+                res.render('questionnaire', {
+                    title: 'Questionnaire',
+                    data: data,
+                    voteAdded,
+                    question,
+                    isVoteButton,
+                    sameUser: 1,
+                    sum,
+                });
+            }
+        });
+    } else {
+
+        voteAdded = "";
+        console.log(isVoteButton);
+        QuestionnairesModel.find({}, (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Dotarłem też tutaj');
+                isVoteButton = true;
+                res.render('questionnaire', {
+                    title: 'Questionnaire',
+                    data,
+                    voteAdded,
+                    question,
+                    isVoteButton,
+                    sameUser: 2
+                });
+            }
+        });
+    }
 });
+
 
 router.post('/questionnaire', (req, res) => {
     const id = req.body.answerId;
@@ -32,6 +69,7 @@ router.post('/questionnaire', (req, res) => {
     }
 
     if (id === undefined) {
+        console.log('AAAAA');
         res.redirect('/questionnaire');
         return;
     }
