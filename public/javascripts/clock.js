0362837456581603cb3408aa18d34cb51ecdaa92
MarcoRy <rydzewski.marek86@gmail.com>
2019-11-15 22:52:02 +0100

    Added clock.js, in css implemented new values for clock. Implemented JS file with clock and counter to index.pug

---
+++ public/javascripts/clock.js
@@ -0,0 +1,24 @@
+function counting()
+{
+    let dzisiaj = new Date();
+
+    let dzien = dzisiaj.getDate();
+    let miesiac = dzisiaj.getMonth()+1;
+    let rok = dzisiaj.getFullYear();
+
+    let godzina = dzisiaj.getHours();
+    if (godzina<10) godzina = "0"+godzina;
+
+    let minuta = dzisiaj.getMinutes();
+    if (minuta<10) minuta = "0"+minuta;
+
+    let sekunda = dzisiaj.getSeconds();
+    if (sekunda<10) sekunda = "0"+sekunda;
+
+    document.getElementById("clock").innerHTML =
+        dzien+"/"+miesiac+"/"+rok+"  "+godzina+":"+minuta+":"+sekunda;
+    
+}
+
+setInterval('counting()'
+,1000);
